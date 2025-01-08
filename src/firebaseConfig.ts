0a371f2c2345ea7c0d3bf503adc27018a817e71e
theoxav <theoxxav@outlook.fr>
2025-01-08 16:42:58 +0100

    Install shadcn/ui and tailwindcss

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -15,4 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
